3ffe7070e1d9e99d9edb8a1f217d6c453d98bf1a
CarlaPerezGavilan <42215073+CarlaPerezGavilan@users.noreply.github.com>
2022-04-04 21:28:47 -0500

    correction paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./styles.css";
-import PersistentDrawerLeft from "./Componentes/Drawer";
+import PersistentDrawerLeft from "../public/Components/Drawer";
 import { useState } from "react";
-import Misfunciones from "./Funciones/Misfunciones";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 
 export default function App() {
-
   return (
     <div className="App">
       <PersistentDrawerLeft />
