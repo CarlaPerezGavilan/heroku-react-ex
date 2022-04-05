251bd30e6a71f4df4f0fd07d4b93cf66bab61ca4
Perez Gavilan <Carla.PerezGavilan@Bain.com>
2022-04-04 21:10:56 -0500

    added drawer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import PersistentDrawerLeft from "./Componentes/Drawer";
+import { useState } from "react";
+import Misfunciones from "./Funciones/Misfunciones";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersistentDrawerLeft />
     </div>
   );
 }
-
-export default App;
